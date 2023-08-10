978e18a18d977b46317ed133f0fae9c25072a4bc
Nicolas AMBRY <nicolas.ambry@atos.net>
2023-08-10 15:09:23 +0200

    Half done

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nb: i64) -> i64 {
+    if (nb > 40) {
+        nb
+    } else {
+        nb * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
